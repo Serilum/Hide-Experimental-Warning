caa591f17a801deb987ea611d03342245317c90f
Rick South <rick@natamus.com>
2024-05-21 23:06:42 +0200

    (v1.1) The mod now also removes the experimental warning from the world list correctly.

---
+++ Common/src/main/java/com/natamus/hideexperimentalwarning/mixin/LevelSummaryMixin.java
@@ -0,0 +1,15 @@
+package com.natamus.hideexperimentalwarning.mixin;
+
+import net.minecraft.world.level.storage.LevelSummary;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = LevelSummary.class, priority = 1001)
+public class LevelSummaryMixin {
+	@Inject(method = "isExperimental", at = @At(value = "RETURN"), cancellable = true)
+	public void isExperimental(CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(false);
+	}
+}
