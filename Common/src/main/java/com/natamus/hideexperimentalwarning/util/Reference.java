3d14f17d06b80dc38f5c4856abf6713ebaebd97d
Rick South <rick@natamus.com>
2024-07-28 07:29:06 +0200

    (v1.2) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/hideexperimentalwarning/util/Reference.java
+++ Common/src/main/java/com/natamus/hideexperimentalwarning/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.hideexperimentalwarning.util;
 public class Reference {
 	public static final String MOD_ID = "hideexperimentalwarning";
 	public static final String NAME = "Hide Experimental Warning";
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.2";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
